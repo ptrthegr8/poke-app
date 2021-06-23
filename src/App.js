9484cce6b63398bf6e2fc801d510c4516a363ee4
Peter Marsh <p.marsh@mac.com>
2021-06-23 10:25:53 -0400

    add class and function components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import React, { useState, useEffect } from "react";
 
 function App() {
+  const [pokemon, setPokemon] = useState([]);
+  const url = "https://pokeapi.co/api/v2/pokemon/";
+  useEffect(() => {
+    fetch(url)
+      .then((res) => res.json())
+      .then((data) => setPokemon(data.results));
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Hello, Pokemon!</h1>
+      <ul>
+        {pokemon.map((p) => (
+          <li>{p.name}</li>
+        ))}
+      </ul>
     </div>
   );
 }
 
-export default App;
+class PokemonApp extends React.Component {
+  url = "https://pokeapi.co/api/v2/pokemon/";
+  constructor(props) {
+    super(props);
+    this.state = { pokemon: [] };
+  }
+  componentDidMount() {
+    fetch(this.url)
+      .then((res) => res.json())
+      .then((data) => this.setState({ pokemon: data.results }));
+  }
+  render() {
+    return (
+      <div>
+        <h1>Hello, Pokemon!</h1>
+        <ul>
+          {this.state.pokemon.map((p) => (
+            <li>{p.name}</li>
+          ))}
+        </ul>
+      </div>
+    );
+  }
+}
+
+export default PokemonApp;
